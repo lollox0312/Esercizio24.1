c68bed277942ad8ad042660f2fb96d8823575e0b
Asus <Asus@LAPTOP-IVBA85K7>
2023-11-28 20:02:57 +0100

    Aggiungere i file di progetto.

---
+++ 24.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato q = new Quadrato();
+            Console.WriteLine("inserisci il lato del quadrato");
+            q.Lato = float.Parse(Console.ReadLine());
+            Console.WriteLine("la superfice del quadrato è: {0}", q.Lato*q.Lato);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 24.1/Quadrato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    internal class Quadrato
+    {
+        public float Lato { get; set; }
+    }
+}
